12ffd2be8efd810e5ee94a3c5eab56e54d51787a
kgueorgu <kgueorgu@ryerson.ca>
2021-08-19 22:14:18 -0400

    Modify python test bench

--- MedianMaintenance/Python/medianMaintenanceTestBench.py
+++ MedianMaintenance/Python/medianMaintenanceTestBench.py
@@ -1,19 +1,21 @@
 import numpy as np
 import math
 
+# Test bench for optimized heap based Median Maintenance (see Java).
+
 # File with all integers
-file1 = open('/home/kliment/Documents/github/StanfordAlgorithms/Assign6Q2/median.txt', 'r')
-Lines = file1.readlines()
+file_handler = open('./median.txt', 'r')
+lines_in_file = file_handler.readlines()
 
 # Numpy array to hold the integers
 data = np.array( [], dtype=np.int_ )
 
 # Read file and fill data array
-for line in Lines:
+for line in lines_in_file:
    number = line.split('\n')[0]
-   data = np.append(data,[ np.int_(number) ])
+   data = np.append(data, [ np.int_(number) ] )
 
-mArray = np.array([])
+mArray = np.array( [] )
 mTotal = 0
 for i in data:
    mArray = np.append( mArray, [ i ] )
