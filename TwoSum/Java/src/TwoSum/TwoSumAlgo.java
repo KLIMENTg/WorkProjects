16939dfbb8083db4bd78f126b000fc84eee24c16
kgueorgu <kgueorgu@ryerson.ca>
2021-08-19 22:52:24 -0400

    Comment 2-sum algorithm minor.

--- TwoSum/Java/src/TwoSum/TwoSumAlgo.java
+++ TwoSum/Java/src/TwoSum/TwoSumAlgo.java
@@ -1,6 +1,6 @@
 /*
  * This program implements the Two Sum Algorithm and makes use
- * of the data structure Hash Table, which provides fast lookup
+ * of the Hash table data structure, which provides fast lookup
  * achieved in constant O(1) time. The goal is to determine how
  * many distinct integers x and y exist within the Hash Table
  * such that, when added together x+y=t, where t is in the
@@ -21,26 +21,28 @@ import java.util.*;
 
 public class TwoSumAlgo{
 	
-	int[] tarray;
-	HashSet <Long> intSet;
+	int[] inputSummationRange;
+	HashSet <Long> integerSet;
+	final int maxSearchRange = 10000;
+	final int minSearchRange = -10000;
 	
 	/*
 	 * This function imports the data from an input file which contains
 	 * 1 million integers, both positive and negative. It then loads it
-	 * into a Hash Table called "intSet", which in Java is of type
-	 * HashSet <Long>
+	 * into a Hash Table called "integerSet", which in Java is of type
+	 * HashSet <Long>. Note some integers need 64 bits, hence Long.
 	 */
 	public void importDataIntoHashSet() {
-		intSet = new HashSet<Long>();
+		integerSet = new HashSet<Long>();
 		try {
-			 File myObj = new File("./src/TwoSum/data.txt");
-		     Scanner myReader = new Scanner(myObj);
-		     while (myReader.hasNextLine()) {
-		        String data = myReader.nextLine();
-		        long a = Long.parseLong(data);
-		        intSet.add(a);
+			 File fileHandler = new File("./src/TwoSum/data.txt");
+		     Scanner fileScanner = new Scanner(fileHandler);
+		     while (fileScanner.hasNextLine()) {
+		        String data = fileScanner.nextLine();
+		        long fileInteger = Long.parseLong(data);
+		        integerSet.add(fileInteger);
 		      }
-		      myReader.close();
+		      fileScanner.close();
 		} catch (FileNotFoundException e) {
 		      System.out.println("An error occurred.");
 		      e.printStackTrace();
@@ -48,51 +50,50 @@ public class TwoSumAlgo{
 	}
 	
 	/*
-	 * This function generates the array "t"
+	 * This function generates the array "inputSummationRange"
 	 * which has a range [-10 000,10 000]
 	 */
 	public void populateSumArr() {
-		tarray = new int[20001];
-		for(int i = -10000; i<=10000; i++) {
-			tarray[i+10000] = i;
+		inputSummationRange = new int[ 20001 ];
+		for(int rangeIdx = minSearchRange; rangeIdx <= maxSearchRange; rangeIdx++) {
+			inputSummationRange[ rangeIdx + maxSearchRange ] = rangeIdx;
 		}
 	}
 	
 	/*
-	 * This is the 2-SUM algorithm
+	 * 2-SUM algorithm. Description top of file.
 	 */
 	public void twoSumAlgorithm() {
 		
-        Iterator<Long> itr=this.intSet.iterator();    
-        long lookup = 0;
+        Iterator<Long> hashSetIterator = this.integerSet.iterator();    
+        long pairLookup = 0;
         int totalOccurences = 0;
         
-        for(int i=0; i<20001; i++) {
+        for(int targetIdx = 0; targetIdx < 20001; targetIdx++) {
 
-	        while(itr.hasNext()){
-	        	long x = itr.next();
-	        	lookup = tarray[i] - x;
-	        	if(intSet.contains(lookup) && lookup != x) {
+	        while(hashSetIterator.hasNext()){
+	        	long x = hashSetIterator.next();
+	        	pairLookup = inputSummationRange[ targetIdx ] - x;
+	        	
+	        	if( integerSet.contains(pairLookup) && pairLookup != x ) { // x,y found s.t. x+y = TargetValue
 	        		totalOccurences++;
 	        		break;
 	        	}
 	        }
-	        itr = this.intSet.iterator();
-	        System.out.println("Running Total: " + totalOccurences + " for T: " + i);
+	        hashSetIterator = this.integerSet.iterator();
+	        System.out.println("Running Total: " + totalOccurences + " for T: " + targetIdx);
         }
         System.out.print("Pairs x&y that sum up to t: " + totalOccurences + " \n");
 	}
 	
 	public static void main(String[] args) {
 		
-		TwoSumAlgo obj = new TwoSumAlgo();
+		TwoSumAlgo twoSumObj = new TwoSumAlgo();
 		
-		obj.importDataIntoHashSet();
-		obj.populateSumArr();
-		obj.twoSumAlgorithm();
+		twoSumObj.importDataIntoHashSet();
+		twoSumObj.populateSumArr();
+		twoSumObj.twoSumAlgorithm();
 
 	}
-	
-	
 }
 
